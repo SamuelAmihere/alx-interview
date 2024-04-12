f14fa42833904119309a7ffe30fb02d1188e9cfb
Samuel Amihere <amisam2000@gmail.com>
2024-04-12 14:00:20 +0800

    Pascal's triangle: Python

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Pascal's Triangle
+"""
+
+def pascal_triangle(n):
+    """Pascal's Triangle: Python implementation"""
+    if type(n) is not int:
+        raise TypeError("n must be an integer")
+    if n <= 0:
+        return []
+    pascal = []
+    for i in range(n):
+        row = [1]
+        if pascal:
+            last_row = pascal[-1]
+            row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
+            row.append(1)
+        pascal.append(row)
+    return pascal
