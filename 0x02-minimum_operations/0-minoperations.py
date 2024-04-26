e22aa0c7771ea580498eecd8130cc303701b158a
Samuel Amihere <amisam2000@gmail.com>
2024-04-26 10:33:02 +0800

    Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""AI is creating summary for
+a text file with a single character H
+"""
+
+
+def minOperations(n):
+    """Perorming Minimum operations"""
+    if type(n) is not int or type(n) is float:
+        return 0
+    if n <= 1:
+        return 0
+    for i in range(2, n + 1):
+        if n % i == 0:
+            return minOperations(n // i) + i
+    return 0
