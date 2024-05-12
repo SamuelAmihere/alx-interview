fb9be1b692bc7dcc7183618fef916b1a2d392cae
Samuel Amihere <amisam2000@gmail.com>
2024-05-12 11:47:24 +0800

    update

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+UTF-8 Validation:
+"""
+
+
+def validUTF8(data):
+    """
+    Determines if a given data set represents a valid
+    UTF-8 encoding.
+    """
+    if not data:
+        return False
+
+    n_bytes = 0
+    i = 0
+
+    while i < len(data):
+        num = data[i]
+        bin_rep = format(num, '#010b')[-8:]
+
+        if n_bytes == 0:
+            j = 0
+            while j < len(bin_rep):
+                # Count the number of 1s in the beginning of the string.
+                # E.g. '11110xxx' should return 4
+                if bin_rep[j] == '0':
+                    break
+                n_bytes += 1
+                j += 1
+
+            if n_bytes == 0:
+                i += 1
+                continue
+
+            if n_bytes == 1 or n_bytes > 4:
+                # UTF-8 characters are between 2 and 4 bytes long
+                return False
+        else:
+            # check if the most significant bit is 1 and the second one is 0
+            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
+                return False
+
+        # Decrease the number of bytes to process
+        n_bytes -= 1
+        i += 1
+
+    return n_bytes == 0
