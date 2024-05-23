ccf00423d826d1c0601f1d1fc26526c9d6830cd9
Samuel Amihere <amisam2000@gmail.com>
2024-05-23 09:25:45 +0800

    Star Wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,45 +2,45 @@
 const request = require('request');
 const API_URL = 'https://swapi.dev/api/films';
 
-function fetchCharacterNames(characterUrls) {
-    return characterUrls.map(url => {
-        return new Promise((resolve, reject) => {
-            request(url, (error, _, body) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    const characterData = JSON.parse(body);
-                    resolve(characterData.name);
-                }
-            });
-        });
+function fetchCharacterNames (characterUrls) {
+  return characterUrls.map((url) => {
+    return new Promise((resolve, reject) => {
+      request(url, (error, _, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          const characterData = JSON.parse(body);
+          resolve(characterData.name);
+        }
+      });
     });
+  });
 }
 
 if (process.argv.length > 2) {
-    const movieId = process.argv[2];
-    const movieUrl = `${API_URL}/${movieId}/`;
+  const movieId = process.argv[2];
+  const movieUrl = `${API_URL}/${movieId}/`;
 
-    request(movieUrl, (err, _, body) => {
-        if (err) {
-            console.error('An error occurred:', err);
-            return;
-        }
+  request(movieUrl, (err, _, body) => {
+    if (err) {
+      console.error('An error occurred:', err);
+      return;
+    }
 
-        const movieData = JSON.parse(body);
-        const characterUrls = movieData.characters;
+    const movieData = JSON.parse(body);
+    const characterUrls = movieData.characters;
 
-        const characterPromises = fetchCharacterNames(characterUrls);
+    const characterPromises = fetchCharacterNames(characterUrls);
 
-        Promise.all(characterPromises)
-            .then(names => {
-                console.log(names.join('\n'));
-            })
-            .catch(error => {
-                console.error('An error occurred:', error);
-            });
-    });
+    Promise.all(characterPromises)
+      .then((names) => {
+        console.log(names.join('\n'));
+      })
+      .catch((error) => {
+        console.error('An error occurred:', error);
+      });
+  });
 } else {
-    console.error('Please provide a movie ID as a positional argument.');
-    process.exit(1);
+  console.error('Please provide a movie ID as a positional argument.');
+  process.exit(1);
 }
