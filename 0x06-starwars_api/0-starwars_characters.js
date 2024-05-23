b390a69eff8ab62a20ece6cf87e5fa17efd7fe2e
Samuel Amihere <amisam2000@gmail.com>
2024-05-23 09:51:01 +0800

    Star Wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api/films';
+const API_URL = 'https://swapi.dev/api/films';
 
 function fetchCharacterNames(characterUrls) {
   return characterUrls.map((url) => {
